1c14033f4b63ca1e54254a10a739f8e12c9be777
Anne Bonfante <bonfante.anne@gmail.com>
2023-10-12 18:18:10 +0200

    receiving infos from the back

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card
+      class="my-card"
+      :color="color"
+      @click="$router.push('/product/' + slug)"
+    >
+      <q-card-section>
+        {{ name }}
+        {{ color }}
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  name: {
+    type: String,
+    default() {
+      return ''
+    }
+  },
+  color: {
+    type: String,
+    default() {
+      return ''
+    }
+  },
+  description: {
+    type: String,
+    default() {
+      return ''
+    }
+  },
+  picture: {
+    type: String,
+    default() {
+      return ''
+    }
+  }
+})
+
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+
+</style>
+
+
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,31 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <category-card
+      v-for="category in info"
+      :key="category.id"
+      :name="category.Nom"
+      :color="category.Couleur"
+      :description="category.Description"
+      :picture="category.Image"
+    >
+    </category-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+import { api } from 'boot/axios'
+import {ref, onMounted } from 'vue'
+import CategoryCard from 'src/components/CategoryCard.vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+const info = ref()
+
+
+onMounted(async () => {
+  console.log('load')
+      api
+      .get('https://la-tournee-web-dev.osc-fr1.scalingo.io/web/stores/1657031023080x131691197342183490/categories')
+      .then(response => (info.value = response.data))
+})
+
+
 </script>
